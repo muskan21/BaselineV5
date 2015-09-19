ab0979c0165b35ecc5cbce363b55956e5f643d37
muskan21 <muskan.cs.du.2013@gmail.com>
2015-09-19 08:31:14 +0000

    Refactored code to remove extra functionality

--- src/com/twu/baseline/Grid.java
+++ src/com/twu/baseline/Grid.java
@@ -9,26 +9,6 @@ public class Grid {
         this.grid = grid;
     }
 
-    public boolean compare(int cellPosition, String neighborPosition) {
-        if("-".equals(grid.get(0).get(1)) && "right".equals(neighborPosition))
-            return false;
-        else if("left".equals(neighborPosition))
-            return false;
-        else if ("up".equals(neighborPosition))
-            return false;
-        else if ("-".equals(grid.get(1).get(0)) && "down".equals(neighborPosition))
-            return false;
-        else if ("-".equals(grid.get(1).get(1)) && "downright".equals(neighborPosition))
-            return false;
-        else if ("upright".equals(neighborPosition))
-            return false;
-        else if ("upleft".equals(neighborPosition))
-            return false;
-        else if ("downleft".equals(neighborPosition))
-            return false;
-        return true;
-    }
-
     public int countAliveNeighbors(int cellRowPosition, int cellColumnPosition) {
         int count = 0;
         if("X".equals(grid.get(cellRowPosition).get(cellColumnPosition + 1)))
