48ba5c5b987364e9e47d8f80d3f2acb1cbf8e4a5
muskan21 <muskan.cs.du.2013@gmail.com>
2015-09-19 06:33:34 +0000

    Compare a cell to its right horizontal neighbor and return true

--- src/com/twu/baseline/Grid.java
+++ src/com/twu/baseline/Grid.java
@@ -1,5 +1,15 @@
 package com.twu.baseline;
 
-public class Grid {
+import java.util.ArrayList;
 
+public class Grid {
+    private ArrayList<ArrayList<String>> grid = new ArrayList<ArrayList<String>>();
+
+    public Grid(ArrayList<ArrayList<String>> grid) {
+        this.grid = grid;
+    }
+
+    public boolean compare(int cellPosition, int neighborPosition) {
+        return true;
+    }
 }
--- test/com/twu/baseline/GridTest.java
+++ test/com/twu/baseline/GridTest.java
@@ -1,7 +1,26 @@
 package com.twu.baseline;
 
-/**
- * Created by muskandhanda on 9/19/15.
- */
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import static org.junit.Assert.assertTrue;
+
 public class GridTest {
+
+    @Test
+    public void shouldCompareFirstMemberCellToItsHorizontallyRightNeighborAndReturnTrueIfItIsX() {
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream("XX\nXX".getBytes());
+        Scanner scanner = new Scanner(byteArrayInputStream);
+        ConsoleInput consoleInput = new ConsoleInput(scanner);
+        ApplicationInput applicationInput = new ApplicationInput(consoleInput);
+        ArrayList<ArrayList<String>> gridInput = applicationInput.getGridInput();
+        Grid grid = new Grid(gridInput);
+
+        boolean result = grid.compare(0, 1);
+
+        assertTrue(result);
+    }
 }
